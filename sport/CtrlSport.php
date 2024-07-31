085517737a8185cd7f75f400c82daadb03148754
Florian M <Florian M>
2024-07-31 15:58:52 +0200

    Co-authored-by: FabienBarbet <FabienBarbet@users.noreply.github.com> Co-authored-by: LebretonJordan <LebretonJordan@users.noreply.github.com> Co-authored-by: Da <Heliuos@users.noreply.github.com>

--- sport/CtrlSport.php
+++ sport/CtrlSport.php
@@ -1,41 +1,38 @@
-<?php 
+<?php
 
-class CtrlSport {
+class CtrlSport
+{
 
-    private $vue;
-    private $model;
+  private $vue;
+  private $model;
 
-    public function __construct()
-    {
-        $this->vue = new ViewSport();
-        $this->model = new ModelSport();
-    }
-
-    function getSport()
-    {
-      if (isset($_GET['sport'])) {
-        $sport = $_GET['sport'];
-        $data = $this->model->selectSport(Conf::$urlSport,$sport);
-        $data2 = $this->model->selectSport(Conf::$urlSport2,$sport);
-        
-       
-          $this->vue->afficherSport($data,$data2);
-          
-       
-      }
-    
-      
-    }
-    function apiSport(){
-      $data = $this->model->selectSport(Conf::$urlSport, "");
-    $widget =[ $data['channel']['item'][0],$data['channel']['item'][1],$data['channel']['item'][2]];
-   
-   // var_dump($widget);
-  $json = json_encode($widget);
-  echo $json;  
+  public function __construct()
+  {
+    $this->vue = new ViewSport();
+    $this->model = new ModelSport();
   }
-  
 
+  function getSport()
+  {
+    if (isset($_GET['sport'])) {
+      $sport = $_GET['sport'];
+    //  var_dump($sport);
+      $data = $this->model->selectSport(Conf::$urlSport, $sport);
+      if ($sport === 'volley-ball') { 
+        $data2 = $this->model->selectSport(Conf::$urlSport2, 'volley');
+      } else {
+        $data2 = $this->model->selectSport(Conf::$urlSport2, $sport);
+      }
+      $this->vue->afficherSport($data, $data2);
+    }
+  }
+  function apiSport()
+  {
+    $data = $this->model->selectSport(Conf::$urlSport, "");
+    $widget = [$data['channel']['item'][0], $data['channel']['item'][1], $data['channel']['item'][2]];
+
+    // var_dump($widget);
+    $json = json_encode($widget);
+    echo $json;
+  }
 }
-
-
